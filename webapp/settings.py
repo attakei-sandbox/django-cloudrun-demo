f68a93217e4fd9375f11878253756b87cc79d12d
Kazuya Takei <attakei@gmail.com>
2020-01-11 19:41:53 +0900

    Manage db in var folder

--- webapp/settings.py
+++ webapp/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(default=f'sqlite:///{BASE_DIR}/db.sqlite3'),
+    'default': env.db(default=f'sqlite:///{BASE_DIR}/var/db.sqlite3'),
 }
 
 
